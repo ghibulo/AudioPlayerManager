25935812ac7ed3142d797dd0f9a671afe1ff8b50
ghibulo <jirka.hrbek@gmail.com>
2020-11-02 23:54:36 +0100

    feat: attempt prepare synchronization

--- tomb.dart
+++ tomb.dart
@@ -4,6 +4,56 @@ import 'dart:io';
 import 'dart:collection'; 
 import 'dart:math';
 
+List decodeTrackName(String trackName) {
+
+    List listTrackName = trackName.split('=');
+
+    if (listTrackName.length != 3) {
+        print("the file ${trackName} don't have format of AudioPlayerManager");
+        return null;
+    }
+
+    int order;
+    try {
+          order = int.parse(listTrackName[0]);
+       } 
+       on FormatException { 
+          print("problem to convert order of the file ${listTrackName[2]} to int");
+          return null;
+       } 
+    
+    return [order, listTrackName];
+}
+
+String encodeTrackName(List<String> indexAlbumName) {
+
+    if (indexAlbumName.length < 3) {
+        throw new Exception("${indexAlbumName} can't be encoded!");
+    }
+    return "${indexAlbumName[0]}=${indexAlbumName[1]}=${indexAlbumName[2]}"
+}
+
+
+List getContentPlayer (String playerPathName) {
+
+    var dir = new Directory(playerPathName);
+    List contents = dir.listSync();
+    Map<String,List<int>> result1 = {};
+    Map<int,List<String>> result2 = {};
+    for (var fileOrDir in contents) {
+        if (fileOrDir is File) {
+            String trackName = fileOrDir.path.split('/').last;
+            print(trackName);
+            List listTrack = decodeTrackName(trackName);
+            if (listTrack != null) {
+                result1[listTrack[1][2]] = (result1[listTrack[1][2]] == null)? [listTrack[0]] : result1[listTrack[1][2]].add(listTrack[0]); 
+                result2[listTrack[0]] = listTrack[1];
+            }
+        } 
+    }
+    return [result1, result2];
+}
+
 
 Map getOnAlbums(Map allAlbums, String order) {
 
@@ -118,19 +168,74 @@ List<int> getPermutation(int infimum, int supremum) {
     return result;
 }
 
+Future<File> moveFile(File sourceFile, String newPath) async {
+    try {
+      // prefer using rename as it is probably faster
+      return await sourceFile.rename(newPath);
+    } on FileSystemException catch (e) {
+      // if rename fails, copy the source file and then delete it
+      final newFile = await sourceFile.copy(newPath);
+      await sourceFile.delete();
+      return newFile;
+    }
+}
+
+
+void renameTrack(String path, String name, List configTrack) {
+
+    File renamedFile = new File("${path}/${name}");
+    String newPathName = "${path}/${encodeTrackName(configTrack)}"
+    try {
+
+        await renamedFile.rename("${newPathName}");
+        print("Track ${name} renamed according ${configTrack}");
+
+    } on FileSystemException catch (e) {
+        File sourceFile = new File("${configTrack[3]}/${encodeTrackName(configTrack)}");
+        final newFile = await sourceFile.copy(newPathName);
+        print("Track ${configTrack} has to be copied to ${path}");
+    }
+
+}
+
+
+void synchronize(Map<int,List<String>> wishLine, String mountFolder) {
+
+    List contentPlayer = getContentPlayer (configData["mount_folder"]);
+
+    Map<String,List<int>> testExistMap = contentPlayer[0];
+    Map<int,List<String>> dataMap = contentPlayer[1];
+
+    for (var item in wishLine.keys) {
+        List<String> configTrack = wishLine[item];
+        if (testExistMap[configTrack[2]] != null) {
+            for (index in testExistMap[configTrack[2]) { //the same trackName in different albums
+                List<String> playerTrack = dataMap[index];
+                if (playerTrack[1] == configTrack[1]) { //albums equal
+                   renameTrack("${mountFolder}/${encodeTrackName(playerTrack)}", configTrack); 
+                }
+
+            }
+        }
+        
+    }
+    
+
+}
+
 void main() {
 
 
   new File('sfsconfig.toml').readAsString().then((String contents) {
 	var parser = new TomlParser();
-    Map moje = new Map.from(parser.parse(contents).value);
-	//print(moje);
-    Map<int,List<String>> theLine = createSongsLine(moje);
-    for (var item in theLine.keys) {
-        List<String> name = theLine[item];
+    Map configData = new Map.from(parser.parse(contents).value);
+    Map<int,List<String>> wishLine = createSongsLine(configData);
+    for (var item in wishLine.keys) {
+        List<String> name = wishLine[item];
         print("${item} - ${name} \n");
     }
 	print("----");
+    syncronize(wishLine, configData["mount_folder"]);
   });
 
 /*
